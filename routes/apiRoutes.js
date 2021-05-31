870ff14fd7c6fdc10ebfd1bec93e7423f74c4b68
Abdale Hassan <cabdale989@gmail.com>
2021-05-30 21:50:43 -0500

    add a get router to read the db json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,17 @@
 const fs = require('fs');
 const { v4: uniqueId } = require('uuid');
 let ArrayOfObj = require('../db/db.json');
+
+
+module.exports = (app) => {
+
+    app.get('/api/notes', (req, res) => { 
+        try {
+            const data = fs.readFileSync('db/db.json', 'utf-8')
+            res.json(JSON.parse(data))
+          } catch (err) {
+            console.error(err)
+          }
+    })
+
+};
