e8fb726aedcd66b9c4fd5567106263e259129bcc
Abdale Hassan <cabdale989@gmail.com>
2021-05-30 20:31:03 -0500

    require express and add middleware

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.urlencoded({extended: true,}));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Application listening on PORT: ${PORT}`);
+});
 
